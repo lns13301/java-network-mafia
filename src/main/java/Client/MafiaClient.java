8d41e1061786a001bcaa2b9f6d24843761341e1f
lns13301 <rkdrbgks22@ks.ac.kr>
2020-08-05 14:47:44 +0900

    feat: Add Client thread
    - 클라이언트의 쓰레드 실행 추가

--- src/main/java/Client/MafiaClient.java
+++ src/main/java/Client/MafiaClient.java
@@ -1,13 +1,24 @@
 package Client;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 
-public class MafiaClient {
+public class MafiaClient extends Thread{
     private static final String SERVER_IP = "127.0.0.1";
     private static final int SERVER_PORT = 2345;
     private Socket socket;
 
     public MafiaClient() {
         socket = new Socket();
+    }
+
+    @Override
+    public void run() {
+        try {
+            socket.connect(new InetSocketAddress(SERVER_IP, SERVER_PORT));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
