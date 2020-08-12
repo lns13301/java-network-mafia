423a3c689cb5fde6a276b0f6106e450f3d79fb1c
lns13301 <rkdrbgks22@ks.ac.kr>
2020-08-12 10:45:08 +0900

    feat: Add server receiver thread run
    - 쓰레드 동작을 관리하는 메서드 추가

--- src/main/java/Server/Receiver.java
+++ src/main/java/Server/Receiver.java
@@ -2,6 +2,7 @@ package Server;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -9,6 +10,7 @@ public class Receiver extends Thread{
     private DataInputStream dataInputStream;
     private DataOutputStream dataOutputStream;
     private String id;
+    private String message;
 
     public Receiver(Socket socket) {
         try{
@@ -18,5 +20,22 @@ public class Receiver extends Thread{
         }catch(Exception e){
             System.out.println("예외:"+e);
         }
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (dataInputStream != null) {
+                message = dataInputStream.readUTF();
+                sendMessage();
+            }
+        }
+        catch (IOException e){
+
+        }
+    }
+
+    private void sendMessage() {
+
     }
 }
