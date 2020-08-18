be090b3dcd7153ef187879fad1d0c694a89898fa
lns13301 <rkdrbgks22@ks.ac.kr>
2020-08-18 09:08:26 +0900

    feat: add Client GUI
    - 클라이언트용 GUI 클래스 추가

---
+++ src/main/java/Client/ClientGUI.java
@@ -0,0 +1,40 @@
+package Client;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ClientGUI extends JFrame implements ActionListener {
+    private static final long serialVersionUID = 1L;
+    private JTextArea jTextArea = new JTextArea(40,25);
+    private JTextField jTextField = new JTextField(25);
+    private MafiaClient client = new MafiaClient();
+    private static String id;
+
+    public ClientGUI() {
+        add(jTextArea, BorderLayout.CENTER);
+        add(jTextField, BorderLayout.SOUTH);
+        jTextField.addActionListener(this);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+        setBounds(800, 100, 400, 600);
+        setTitle("MafiaClient");
+
+        //client.setGUI(this);
+        //client.setId(id);
+        //client.connect();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String message = id + " : " + jTextField.getText() + "\n";
+        //client.sendMessage(message);
+        jTextField.setText("");
+    }
+
+    public void appendMessage(String message) {
+        jTextArea.append(message);
+    }
+}
